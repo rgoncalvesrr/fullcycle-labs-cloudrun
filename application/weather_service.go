9aa968195888e0a869253120b08931aca800895a
Ricardo <ricardo@goncalves.biz>
2024-11-13 00:41:12 -0300

    feat: adiciona caso de uso que obtém a temperatura

--- application/weather_service.go
+++ application/weather_service.go
@@ -1,6 +1,16 @@
 package application
 
+import "context"
+
+type WeatherOutput struct {
+	Celsius    float64 `json:"temp_C"`
+	Fahrenheit float64 `json:"temp_F"`
+	Kelvin     float64 `json:"temp_k"`
+}
+
+// https://cep.awesomeapi.com.br/json/09130220
 type IWeatherService interface {
+	GetTemperature(ctx context.Context, cep string) (*WeatherOutput, error)
 }
 
 type weatherService struct {
@@ -16,4 +26,24 @@ func NewWeatherService(
 		coordinateRepository: coordinateRepository,
 		weatherRepository:    weatherRepository,
 	}
+}
+
+func (w weatherService) GetTemperature(ctx context.Context, cep string) (*WeatherOutput, error) {
+	// Tenta obter a latitude e logitude
+	c, e := w.coordinateRepository.GetCoordinate(ctx, cep)
+
+	if e != nil {
+		return nil, e
+	}
+	t, e := w.weatherRepository.GetTemperature(ctx, c)
+
+	if e != nil {
+		return nil, e
+	}
+
+	return &WeatherOutput{
+		Celsius:    t.Celsius(),
+		Kelvin:     t.Kelvin(),
+		Fahrenheit: t.Fahrenheit(),
+	}, nil
 }
